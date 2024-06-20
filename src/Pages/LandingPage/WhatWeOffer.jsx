58b076a9eaf7f6fe099ca1e91dac01071c5b7513
Taha Shah <tahashah411@gmail.com>
2024-06-20 17:01:50 +0500

    better fitting

--- src/Pages/LandingPage/WhatWeOffer.jsx
+++ src/Pages/LandingPage/WhatWeOffer.jsx
@@ -78,7 +78,7 @@ const ServiceTab = ({title, content, illustrationSrc, link, linkText, open, onCl
           <p className="flex-1">{title}</p>
         </div>
 
-        <div className={`flex gap-2 duration-700 transition-all ${open ? "h-72 xs:h-60 sm:h-72 lg:h-[400px] xl:h-[500p  opacity-100" : "h-0 overflow-hidden opacity-0"}`}>
+        <div className={`flex gap-2 duration-700 transition-all ${open ? "h-fit slide-up opacity-100" : "h-0 overflow-hidden opacity-0"}`}>
           <div className="flex flex-1 flex-col gap-2 items-start">
             <p className="text-xs sm:text-base md:text-lg lg:text-sm xl:text-lg">
               <img src={illustrationSrc} className="w-24 p-4 xs:w-32 sm:w-40 md:w-48 lg:w-32 xl:w-64 h-fit float-start" />
