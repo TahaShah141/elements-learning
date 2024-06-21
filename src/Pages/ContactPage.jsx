dc7f6e79158422e5b98f81ce1e91bd5332a4a9d2
Taha Shah <tahashah411@gmail.com>
2024-06-21 14:10:17 +0500

    start contact section

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -1,6 +1,15 @@
+import { Footer } from "../components/Footer"
 
 export const ContactPage = () => {
   return (
-    <div>ContactPage</div>
+    <div className="flex flex-col">
+      <section className="bg-[#F3EEE8] w-full p-4">
+        This is the hero section
+      </section>
+      <section className="w-full p-4">
+        This is the form section
+      </section>
+      <Footer />
+    </div>
   )
 }
