e2a255427f2dfa9d0a9a6809a42b2c9e5b1064be
Taha Shah <tahashah411@gmail.com>
2024-06-21 14:28:20 +0500

    make video section responsive

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -34,10 +34,10 @@ export const LandingPage = () => {
       </section> 
 
       {/* Video Section */}
-       <section className="flex gap-8 px-48 py-10 items-center">
-        <SlidingDiv direction="left" px={50} className="h-48 aspect-video bg-neutral-200 rounded-lg"></SlidingDiv>
+       <section className="flex gap-2 md:gap-8 lg:px-32 xl:px-40 md:p-10 p-4 xs:p-6 items-center flex-col md:flex-row">
+        <SlidingDiv direction="left" px={50} className="w-full md:w-auto md:h-48 aspect-video bg-neutral-200 rounded-lg"></SlidingDiv>
         <SlidingDiv direction="right" px={50}>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi tenetur alias optio maxime eveniet, officia ipsum! Optio cum sequi, ipsam quo est debitis enim officia, doloribus non dignissimos iusto fuga commodi error obcaecati quidem consectetur dolores dolor ad aliquam amet cumque maxime. Minima beatae possimus veniam optio a ullam quasi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore dolor facilis, porro illo harum deleniti provident voluptas magni praesentium distinctio? Explicabo, quas ab! Praesentium, sint.</p>
+          <p className="text-xs xs:text-sm sm:text-base">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi tenetur alias optio maxime eveniet, officia ipsum! Optio cum sequi, ipsam quo est debitis enim officia, doloribus non dignissimos iusto fuga commodi error obcaecati quidem consectetur dolores dolor ad aliquam amet cumque maxime. Minima beatae possimus veniam optio a ullam quasi. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore dolor facilis, porro illo harum deleniti provident voluptas magni praesentium distinctio? Explicabo, quas ab! Praesentium, sint.</p>
         </SlidingDiv>
       </section> 
 
@@ -51,14 +51,14 @@ export const LandingPage = () => {
       </section>
 
       {/* Events Carousel */}
-       <section className="bg-[#F3EEE8] w-full flex justify-center">
+      {/* <section className="bg-[#F3EEE8] w-full flex justify-center">
         <div className="container p-20 flex justify-center">
           <Carousel items={Array.from(Array(5)).map((_, i) =>
             <EventCard title={"Test Event "+(i+1)} date={"12-06-2024"}
             content={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum sit officia aut odit voluptas ullam omnis eveniet ipsa sequi recusandae perspiciatis culpa necessitatibus laudantium reprehenderit quo ab neque quaerat tempore ut vero error, voluptatibus ipsam itaque. Officia unde cumque dolorum quo nesciunt omnis asperiores, perferendis nulla soluta. Molestiae commodi perspiciatis sit saepe omnis porro mollitia natus, illo aliquid fuga reiciendis velit explicabo quam accusamus enim animi. Iusto quod, ipsam aut illum reprehenderit nam nulla sequi aperiam eligendi voluptatibus fugit magnam ut, totam architecto praesentium tempore, doloremque nisi at cupiditate ratione consequuntur natus. Nesciunt iusto illum cumque odit molestias expedita dolorem."} />
             )} />
         </div>
-      </section>
+      </section> */}
 
       <section className="w-full p-20 flex justify-center gap-12">
         <Testimonials />
