312b77a6bcbdc5d50daf7fd1b6ce77b41d34f936
Taha Shah <tahashah411@gmail.com>
2024-06-25 10:18:49 +0500

    changes

--- src/Pages/LandingPage/WhatWeOffer.jsx
+++ src/Pages/LandingPage/WhatWeOffer.jsx
@@ -78,12 +78,12 @@ const ServiceTab = ({title, content, illustrationSrc, link, linkText, open, onCl
           <p className="flex-1">{title}</p>
         </div>
 
-        <div className={`flex gap-2 duration-700 transition-all ${open ? "h-72 xs:h-64 min-[500px]:h-48 sm:h-72 xl:h-96 opacity-100" : "h-0 overflow-hidden opacity-0"}`}>
+        <div className={`flex flex-col items-start gap-2 duration-700 transition-all ${open ? "h-72 xs:h-64 min-[500px]:h-48 sm:h-72 xl:h-96 opacity-100" : "h-0 overflow-hidden opacity-0"}`}>
           <p className="text-xs sm:text-base md:text-lg lg:text-sm xl:text-base">
             <img src={illustrationSrc} className="w-24 p-4 xs:w-32 sm:w-40 md:w-48 lg:w-32 xl:w-64 h-fit float-start" />
             {content}
-            <LinkButton to={link} text={linkText} textColor="#FBBA41" bgColor="#353535" />
           </p>
+          <LinkButton to={link} text={linkText} textColor="#FBBA41" bgColor="#353535" />
         </div>
       </div>
     </>
