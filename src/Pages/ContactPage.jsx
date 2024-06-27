ae3c5d74874a2cb920ce0b9d39b05f1c98369ca4
Umair Muhammad <umairijaz2772@gmail.com>
2024-06-27 15:24:14 +0500

    Add info heading to contactpage

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -31,6 +31,7 @@ export const ContactPage = () => {
             <button className="bg-[#FBBA41] rounded-md p-2 sm:text-base text-sm sm:p-4 font-bold">Submit</button>
           </SlidingDiv>
           <SlidingDiv direction={"right"} className="flex-[1.5_0_0] flex flex-col gap-4">
+          <h1 className=" font-bold text-3xl text-center sm:hidden">Info</h1>
             <div className="flex gap-2 p-4 md:p-6 rounded-md bg-[#F3EEE8]">
               <svg xmlns="http://www.w3.org/2000/svg" className="size-8" viewBox="0 0 24 24"><title>map-marker</title><path d="M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12,2Z" fill="currentColor"/></svg>
               <div className="flex flex-col gap-2">
