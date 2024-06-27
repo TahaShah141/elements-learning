7604560ed241009ad675bfb6c0fd276d307b7007
Taha Shah <tahashah411@gmail.com>
2024-06-28 00:52:13 +0500

    add tabs for what we offer maths page

---
+++ src/Pages/WhatWeOfferPage/Sections/TabView.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+const TabHeader = ({tab, isOpen, onClick}) => {
+  return (
+    <div onClick={onClick} className={`flex-1 h-64 shadow-lg p-6 flex flex-col gap-2 justify-end transition-all duration-300 ${isOpen ? "bg-white" : "bg-white/40 hover:bg-white/70"}`}>
+      <div className="h-3/5">
+        <img src={tab.src} alt="" className="h-full"/>
+      </div>
+      <h3 className="font-bold text-3xl">{tab.name}</h3>
+    </div>
+  )
+}
+
+export const TabView = ({name, color, src, tabs}) => {
+
+  const [opened, setOpened] = useState(tabs[0].name)
+
+  return (
+    <div className="flex flex-col w-full">
+      <div className="flex flex-col p-8 px-16 gap-12" style={{backgroundColor: color}}>
+        <div className="flex w-full justify-between px-4 py-2 items-center border-b-2 border-light-black">
+          <h1 className="font-bold text-8xl">{name}</h1>
+          <img src={src} alt="" className="h-full"/>
+        </div>
+
+        <div className="flex gap-4 justify-center">
+          {tabs.map(tab => <TabHeader tab={tab} onClick={() => setOpened(tab.name)} isOpen={opened === tab.name} />)}
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ src/Pages/WhatWeOfferPage/Sections/sections.jsx
@@ -0,0 +1,30 @@
+export const sections = 
+[
+  {
+    name: "Maths",
+    color: "#FBBA42",
+    src: "/landingpage/maths.png",
+    tabs: [
+      {
+        name: "Publications",
+        src: "/whatweofferpage/maths/publication.png",
+      },
+      {
+        name: "Trainings",
+        src: "/whatweofferpage/maths/trainings.png",
+      },
+      {
+        name: "Maths Lab",
+        src: "/whatweofferpage/maths/maths-lab.png",
+      },
+      {
+        name: "Manipulative",
+        src: "/whatweofferpage/maths/maths-manipulatives.png",
+      },
+      {
+        name: "Assessments",
+        src: "/whatweofferpage/maths/assessments.png",
+      },
+    ]
+  }
+]
