38aec7a4b35e747a5379b82352da9fb4071c752a
Umair Muhammad <umairijaz2772@gmail.com>
2024-06-27 10:28:59 +0500

    landing page content

--- src/Pages/LandingPage/OurJourney/OurJourneySection.jsx
+++ src/Pages/LandingPage/OurJourney/OurJourneySection.jsx
@@ -1,10 +1,10 @@
+import { useEffect, useState } from 'react'
 import { useInView } from 'react-intersection-observer'
 import '../../../Styles/ourJourney.css'
 import { FadeIn } from '../../../components/FadeIn'
 import { SlidingDiv } from '../../../components/SlidingDiv'
 import { Bullet } from './Bullet'
 import { Jahaaz } from './Jahaaz'
-import { useEffect, useState } from 'react'
 
 const animationTime = 3
 
@@ -39,8 +39,8 @@ const getTangentAngle = (xPercent) => {
 export const journeySteps = [
   {
     color: "#EB008B",
-    header: "2017-2019",
-    content: "Conducted formal training for private school while accumulating a vast knowledge base, laying the groundwork for its future endeavors.",
+    header: "2009-2017",
+    content: "Pioneered homeschooling, discovering the stark difference between children's love for learning and the traditional school experience.",
     contentBelow: true
   },
   {
@@ -51,20 +51,20 @@ export const journeySteps = [
   },
   {
     color: "#00ADEF",
-    header: "2017-2019",
-    content: "Conducted formal training for private school while accumulating a vast knowledge base, laying the groundwork for its future endeavors.",
+    header: "2019-2021",
+    content: "Faced resistance to change in existing school systems, realizing the need for broader engagement from government and university sectors.",
     contentBelow: true
   },
   {
     color: "#F3811F",
-    header: "2017-2019",
-    content: "Conducted formal training for private school while accumulating a vast knowledge base, laying the groundwork for its future endeavors.",
+    header: "2021-2023",
+    content: "Persisted in outreach efforts, securing a pivotal partnership with NUST to advance its vision for education reform.",
     contentBelow: false
   },
   {
     color: "#6C3A92",
-    header: "2017-2019",
-    content: "Conducted formal training for private school while accumulating a vast knowledge base, laying the groundwork for its future endeavors.",
+    header: "2023-Present",
+    content: "Emerged as a NUST spinoff, poised to revolutionize K-12 education through research-based parctices and accessible resources.",
     contentBelow: true
   }
 ]
