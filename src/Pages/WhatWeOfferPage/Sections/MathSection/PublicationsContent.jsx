fd84c481f01b61fbcb1d273ae93050e8f0ad2962
Taha Shah <tahashah411@gmail.com>
2024-06-28 16:03:05 +0500

    make publications section

--- src/Pages/WhatWeOfferPage/Sections/MathSection/PublicationsContent.jsx
+++ src/Pages/WhatWeOfferPage/Sections/MathSection/PublicationsContent.jsx
@@ -26,15 +26,18 @@ export const PublicationsContent = () => {
             id: "Textbooks",
             content: 
             <div className="p-8">
-
-            <CarouselSingle containerClass={"w-72 h-96"} nextButton={"next"} prevButton={"prev"}
-            items={[
-              <div className="size-full bg-sky-100"></div>,
-              <div className="size-full bg-sky-200"></div>,
-              <div className="size-full bg-sky-300"></div>,
-              <div className="size-full bg-sky-400"></div>,
-              <div className="size-full bg-sky-500"></div>,
-            ]} />
+              <div className="flex gap-2">
+                <div className="flex-1 flex flex-col text-right p-12 gap-8 bg-[#F3EEE8]">
+                  <h3 className="text-3xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit deserunt quos molestiae, maxime dolor, odit amet nobis iusto autem dolorem recusandae doloremque, animi ratione laboriosam soluta commodi. Ad deserunt possimus voluptatum suscipit veniam dolorum illum fugiat quia molestias, doloribus error!</h3>
+                  <p className="text-base md:text-lg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime architecto voluptates animi beatae maiores exercitationem optio, harum delectus incidunt recusandae minima molestias corrupti qui sapiente ea illum reprehenderit earum magnam alias ullam illo a. Corrupti, expedita in. Architecto, distinctio vero.</p>
+                  <div className="flex gap-2 mt-auto justify-end">
+                    <button className="px-6 py-4 font-bold text-lg text-white bg-[#FBBA42]">Teacher Guide</button>
+                    <button className="px-6 py-4 font-bold text-lg text-white bg-[#353535]">Student Workbooks</button>
+                  </div>
+                </div>
+                <div className="h-[600px] w-[450px] bg-neutral-400">
+                </div>
+              </div>
             </div>
 
           },
@@ -42,9 +45,15 @@ export const PublicationsContent = () => {
             title: "Popular Maths",
             id: "Popular Maths",
             content: 
-            <div className="p-4 bg-red-400">
-              <div className="h-96 bg-sky-400">
-
+            <div className="p-8">
+              <div className="flex gap-2">
+                <div className="h-[600px] w-[450px] bg-neutral-400">
+                </div>
+                <div className="flex-1 flex flex-col p-12 gap-8 text-base md:text-lg bg-[#F3EEE8]">
+                  <h3 className="text-3xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit deserunt quos molestiae, maxime dolor, odit amet nobis iusto autem dolorem recusandae doloremque, animi ratione laboriosam soluta commodi. Ad deserunt possimus voluptatum suscipit veniam dolorum illum fugiat quia molestias, doloribus error!</h3>
+                  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime architecto voluptates animi beatae maiores exercitationem optio, harum delectus incidunt recusandae minima molestias corrupti qui sapiente ea illum reprehenderit earum magnam alias ullam illo a. Corrupti, expedita in. Architecto, distinctio vero.</p>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus porro, illum impedit laborum dignissimos assumenda deleniti voluptatum similique cupiditate exercitationem?</p>
+                </div>
               </div>
             </div>
           },
