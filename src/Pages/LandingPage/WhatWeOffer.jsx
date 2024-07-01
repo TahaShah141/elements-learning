b983eacb77dadff6b7ffc830f179470c255fabc5
Umair Muhammad <umairijaz2772@gmail.com>
2024-07-01 15:41:54 +0500

    add whatweoffer dropdown text

--- src/Pages/LandingPage/WhatWeOffer.jsx
+++ src/Pages/LandingPage/WhatWeOffer.jsx
@@ -6,7 +6,7 @@ import { ArrowRightIcon } from "lucide-react"
 export const services = [
   {
     title: "Early Childhood Education (ECE) and Parenting",
-    content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam nihil unde assumenda iste dolores. Corporis magnam sapiente odit pariatur officia.",
+    content: " Supporting children's growth through targeted Early Childhood Education (ECE) and parenting strategies, ensuring a nurturing environment from preschool to young adulthood.",
     illustrationSrc: "/landingpage/ece.png",
     link: "/services/ece",
     linkText: "Learn more",
@@ -14,7 +14,7 @@ export const services = [
   },
   {
     title: "Maths",
-    content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam nihil unde assumenda iste dolores. Corporis magnam sapiente odit pariatur officia.",
+    content: "Enhancing math education with innovative resources like Maths Labs, interactive corners, and recreational activities, all based on the CPA methodology for deeper understanding.",
     illustrationSrc: "/landingpage/maths.png",
     link: "/services/maths",
     linkText: "Learn more",
@@ -22,7 +22,7 @@ export const services = [
   },
   {
     title: "Language",
-    content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam nihil unde assumenda iste dolores. Corporis magnam sapiente odit pariatur officia.",
+    content: " Promoting multilingual education by starting with the mother tongue and transitioning to additional languages, fostering cultural richness and effective communication skills.",
     illustrationSrc: "/landingpage/language.png",
     link: "/services/language",
     linkText: "Learn more",
@@ -30,7 +30,7 @@ export const services = [
   },
   {
     title: "Science",
-    content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam nihil unde assumenda iste dolores. Corporis magnam sapiente odit pariatur officia.",
+    content: " Making science engaging and hands-on with 'Science Tamasha,' encouraging active participation and curiosity through experiments and interactive learning.",
     illustrationSrc: "/landingpage/science.png",
     link: "/services/science",
     linkText: "Learn more",
@@ -38,7 +38,7 @@ export const services = [
   },
   {
     title: "Social Science",
-    content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam nihil unde assumenda iste dolores. Corporis magnam sapiente odit pariatur officia.",
+    content: "Comprehensive resources and interactive materials covering history, geography, civics, and cultural studies to foster critical thinking and global understanding.",
     illustrationSrc: "/landingpage/socialscience.png",
     link: "/services/socialsciences",
     linkText: "Learn more",
@@ -46,7 +46,7 @@ export const services = [
   },
   {
     title: "STEAM (connected learning)",
-    content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam nihil unde assumenda iste dolores. Corporis magnam sapiente odit pariatur officia.",
+    content: " Integrating history and geography with real-life contexts, our Connected Learning approach builds a holistic understanding of the world and its interconnected elements.",
     illustrationSrc: "/landingpage/steam.png",
     link: "/services/steam",
     linkText: "Learn more",
@@ -54,7 +54,7 @@ export const services = [
   },
   {
     title: "Ed Tech",
-    content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam nihil unde assumenda iste dolores. Corporis magnam sapiente odit pariatur officia.",
+    content: "Leveraging technology to enhance learning through interactive tools, adaptive learning paths, and advanced analytics, bridging traditional and modern education for the 21st century.",
     illustrationSrc: "/landingpage/edtech.png",
     link: "/services/edtech",
     linkText: "Learn more",
