2e9f99921793e69b2e8da4de08ba257b0d039681
Umair Muhammad <umairijaz2772@gmail.com>
2024-07-04 09:01:07 +0500

    Add border and footer's location text

--- src/Pages/Footer/LocationSection.jsx
+++ src/Pages/Footer/LocationSection.jsx
@@ -7,13 +7,11 @@ export const LocationSection = () => {
       <div className="p-2 border-b-2 xl:hidden border-[#FBBA41] font-bold text-white text-lg">Location</div>
       <div className="flex-1 xl:px-4 flex gap-2 flex-col sm:flex-row xl:gap-8 text-white">
         <div className="flex-1 flex-col text-sm md:text-base flex gap-6">
-          <p className="">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam magnam quasi quisquam earum exercitationem. Nulla qui, culpa sit eaque cupiditate cumque sapiente, quo quam, odio nobis laborum. Totam, obcaecati velit.
-          </p>
+          <p className="">We're open Monday to Friday, from 9:00 AM to 5:00 PM. Whether you're visiting us for a meeting or just dropping by to say hello, we're excited to welcome you. If you need any help finding us, please contact us at.</p>
           <div className="flex flex-col gap-4">
             <div className="flex gap-2 items-center xl:px-4">
               <svg className="size-4 xl:size-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M6.62,10.79C8.06,13.62 10.38,15.94 13.21,17.38L15.41,15.18C15.69,14.9 16.08,14.82 16.43,14.93C17.55,15.3 18.75,15.5 20,15.5A1,1 0 0,1 21,16.5V20A1,1 0 0,1 20,21A17,17 0 0,1 3,4A1,1 0 0,1 4,3H7.5A1,1 0 0,1 8.5,4C8.5,5.25 8.7,6.45 9.07,7.57C9.18,7.92 9.1,8.31 8.82,8.59L6.62,10.79Z" fill="currentColor"/></svg>
-              <p className="md:text-lg">0300 0000000</p>
+              <p className="md:text-lg">+92 327 8698770</p>
             </div>
             <div className="flex gap-2 items-center xl:px-4">
               <svg className="size-4 xl:size-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>email</title><path d="M20,8L12,13L4,8V6L12,11L20,6M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z" fill="currentColor"/></svg>
@@ -22,7 +20,7 @@ export const LocationSection = () => {
           </div>
 
           <div className="flex flex-col gap-4">
-            <div className="sm:hidden flex rounded-2xl overflow-hidden h-64 w-full">
+            <div className="sm:hidden flex rounded-lg overflow-hidden h-64 w-full">
               <iframe className="size-full" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3321.501805795956!2d72.98523267424993!3d33.644157339170654!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38df967473fa7055%3A0xc14fdbda9a155fa7!2sIAEC%20Building%2C%20Scholars%20Ave%2C%20H-12%2C%20Islamabad%2C%20Islamabad%20Capital%20Territory%2C%20Pakistan!5e0!3m2!1sen!2s!4v1719986374086!5m2!1sen!2s" width allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>    
             </div>
             <div className="flex justify-center gap-8">
