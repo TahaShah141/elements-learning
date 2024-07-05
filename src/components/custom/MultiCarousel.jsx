e79490b85f97b5181ce1e904b398b535b5d857fd
Taha Shah <tahashah411@gmail.com>
2024-07-05 20:28:42 +0500

    add resizing all

--- src/components/custom/MultiCarousel.jsx
+++ src/components/custom/MultiCarousel.jsx
@@ -100,7 +100,7 @@ export const MultiCarousel = ({items, autoScroll=true, delay=3000}) => {
       </div>
       <div className="relative z-10 h-1/2 w-1 sm:-translate-y-[8%]" style={{width: smallerSize}}>
         {items.map((item, i) => (
-          <div className={`absolute transition-transform duration-700 bottom-0 ${i === index ? "h-full w-full" : "h-2/3 w-2/3"}`} 
+          <div className={`absolute transition-all duration-700 bottom-0 ${i === index ? "h-full w-full" : "h-2/3 w-2/3"}`} 
           style={{backgroundColor: item.innerColor, transform: `${index > i ? `translateX(calc(${100*1/2}% + ${(index-i) * (smallerSize*(2/3) + gapSize)}px))` : `translateX(${(index-i) * (smallerSize*(2/3) + gapSize) }px)`}`}}
           onClick={() => (i === index) ? setOpened(true) : setIndex(i)}
           >
