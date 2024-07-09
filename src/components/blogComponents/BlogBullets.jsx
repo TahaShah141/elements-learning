bccf642dacec6f4df47ab6d48ac60bcc2333ecc3
Taha Shah <tahashah411@gmail.com>
2024-07-09 18:25:46 +0500

    css changes

--- src/components/blogComponents/BlogBullets.jsx
+++ src/components/blogComponents/BlogBullets.jsx
@@ -7,21 +7,25 @@ export const BlogBullets = ({header, points, size="BASE", style="LETTER"}) => {
   const [alignment, setAlignment] = useState("items-center")
   useEffect(() => {
     if (style === "CIRCLE" || style === "DOTS") {
-      setAlignment("items-center")
+      setAlignment("items-start")
     }
     else if (style === "NUMBER" || style === "LETTER") {
       setAlignment("items-start")
     }
-  }, [])
+  }, [style])
 
   const Marker = ({i}) => {
     if (style === "DOTS") {
-      return <div className="bg-light-black rounded-full size-2" />
+      return (
+        <div className="bg-light-black p-0.5 mt-2 rounded-full flex-1">
+          <div className="bg-light-black rounded-full size-1 md:size-2" />
+        </div>
+      )
     } 
     else if (style === "CIRCLE") {
       return (
-        <div className="bg-black p-0.5 rounded-full">
-          <div className="bg-white rounded-full size-2" />
+        <div className="bg-light-black p-0.5 mt-2 rounded-full flex-1">
+          <div className="bg-white rounded-full size-1 md:size-2" />
         </div>
       )
     } else if (style === "NUMBER") {
@@ -38,7 +42,7 @@ export const BlogBullets = ({header, points, size="BASE", style="LETTER"}) => {
   return (
     <div className="flex flex-col gap-1">
       {header && <BlogParagraph size={size} text={header} />}
-      <div className="flex flex-col pl-12 gap-1">
+      <div className="flex flex-col pl-2 xs:pl-4 md:pl-8 xl:pl-12 gap-1">
         {points.map((point, i) => (
           <div className={`flex gap-2 ${alignment}`}>
             <Marker i={i} />
